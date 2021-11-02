7da7084184cccf18b94bc458cfed2b71799adc18
Sammwy <44925968+sammwyy@users.noreply.github.com>
2021-11-02 13:13:40 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,57 @@
+###########################
+#          Nginx          #
+###########################
+# 1.1 Add the source repository.
+echo deb http://nginx.org/packages/debian/buster nginx > /etc/apt/sources.list.d/nginx.list
+deb-src http://nginx.org/packages/debian/buster nginx >> /etc/apt/sources.list.d/nginx.list
+# 1.2 Add the signing key.
+curl -L https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+# 1.3 Update the package sources.
+sudo apt update
+# 1.4 Install or upgrade nginx.
+sudo apt install -y nginx
+sudo apt upgrade
+
+###########################
+#         MariaDB         #
+###########################
+# 2.1 Install MariaDB Server
+sudo apt install -y mariadb-server
+
+###########################
+#           PHP           #
+###########################
+# 3.1 Install FMP (PHP Preprocessor)
+sudo apt install -y php-fpm
+# 3.2 Install PHP MySQL Addon
+sudo apt install -y php-mysql
+
+###########################
+#          Setup          #
+###########################
+# 4.1 Setup Nginx to use PHP
+echo "server {"                                            > /etc/nginx/sites-available/default
+echo "   listen 80 default_server;"                       >> /etc/nginx/sites-available/default
+echo "   listen [::]:80 default_server;"                  >> /etc/nginx/sites-available/default
+echo " "                                                  >> /etc/nginx/sites-available/default
+echo "   root /var/www/html;"                             >> /etc/nginx/sites-available/default
+echo "   index index.php index.html index.htm"            >> /etc/nginx/sites-available/default
+echo " "                                                  >> /etc/nginx/sites-available/default
+echo "   location / {"                                    >> /etc/nginx/sites-available/default
+echo "       try_files \$uri \$uri/ =404;"                >> /etc/nginx/sites-available/default
+echo "   }"                                               >> /etc/nginx/sites-available/default
+echo " "                                                  >> /etc/nginx/sites-available/default
+echo "   location ~ \\.php\$ {"                           >> /etc/nginx/sites-available/default
+echo "       include snippets/fastcgi-php.conf;"          >> /etc/nginx/sites-available/default
+echo "       fastcgi_pass unix:/run/php/php7.3-fpm.sock;" >> /etc/nginx/sites-available/default
+echo "   }"                                               >> /etc/nginx/sites-available/default
+echo " "                                                  >> /etc/nginx/sites-available/default
+echo "   location ~ /\\.ht {"                             >> /etc/nginx/sites-available/default
+echo "       deny all;"                                   >> /etc/nginx/sites-available/default
+echo "   }"                                               >> /etc/nginx/sites-available/default
+echo "}"                                                  >> /etc/nginx/sites-available/default
+
+###########################
+#         Restart         #
+###########################
+sudo systemctl reload nginx
