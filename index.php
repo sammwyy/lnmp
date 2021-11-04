448d9b27e58b4422052c9667132cc559dee925e1
Sammwy <44925968+sammwyy@users.noreply.github.com>
2021-11-04 19:50:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>It's Work!</title>
+</head>
+<body>
+    <h1>LNMP</h1>
+    <p>Using PHP version <?php echo phpversion()?></p>
+</body>
+</html>
